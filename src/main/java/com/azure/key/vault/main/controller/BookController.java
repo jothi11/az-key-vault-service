367084149604f16e89652f1894f1e879b45a76c3
jothi.lakshmi <jothi.lakshmi@happiestminds.com>
2024-02-27 15:17:33 +0530

    Update BookController.java

--- src/main/java/com/azure/key/vault/main/controller/BookController.java
+++ src/main/java/com/azure/key/vault/main/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.azure.key.vault.main.entity.Book;
 import com.azure.key.vault.main.repository.BookRepository;
@@ -18,9 +19,19 @@ public class BookController {
 	
 	@Autowired
 	BookRepository bookRepository;
+
+	@Value("${spring.datasource.username}")
+	private String dbusername;
+	
+	@Value("${spring.datasource.password}")
+	private String dbpassword;
 	
     @GetMapping(value="/all")
     public  ResponseEntity<List<Book>> getBooks() {
+
+			System.out.println("-------- spring.datasource.username : "+dbusername);
+		System.out.println("-------- spring.datasource.password :  "+dbpassword);
+	    
         List<Book> bookList = bookRepository.findAll();
         return new ResponseEntity<List<Book>>(bookList, HttpStatus.OK);
     }
